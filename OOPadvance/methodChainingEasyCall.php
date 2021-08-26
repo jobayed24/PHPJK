d2b777073e2156c046d1fb4613c58bce5b234070
jobayed24 <jkhouse117@gmail.com>
2021-08-26 15:48:09 +0600

    oop method chaining means call all built method easily after one by one it' called method chaining

---
+++ OOPadvance/methodChainingEasyCall.php
@@ -0,0 +1,51 @@
+<?php
+
+class StringUtility{
+    private $string;
+    private $search;
+    function __construct($string)
+    {
+        $this->string=$string;
+    }
+
+    function search($string){
+        $this->search=$string;
+        return $this;
+    }
+    function replace($rstring){
+        if(!$this->search){
+            throw new Exception("Nothing found Data");
+        }else{
+        $this->string= str_replace($this->search,$rstring,$this->string);
+        }
+        
+        return $this;
+        $this->string="";
+        
+        
+
+    }
+    function stringUperCase(){
+       $this->string= strtoupper($this->string);
+        return $this;
+    }
+    function stringLowerCase(){
+      $this->string=  strtolower($this->string);
+        return $this;
+    }
+
+    function echo(){
+        print($this->string);
+    }
+
+}
+
+$su=new StringUtility("hello world");
+
+$su->search("hello")
+->replace("My")
+->search("world")
+->replace("planet")
+->stringLowerCase()
+->stringUperCase()
+->echo();
