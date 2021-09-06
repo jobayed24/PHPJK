bbe3fca1a275ee74ff2fbad5cb2d2bd3993188c8
jobayed24 <jkhouse117@gmail.com>
2021-09-06 20:19:43 +0600

    mailing with smtp with PHPMailer from github

---
+++ mainling/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require_once "PHPMailer/src/PHPMailer.php";
+require_once "PHPMailer/src/Exception.php";
+require_once "PHPMailer/src/SMTP.php";
+
+
+$pm=new  PHPMailer(true);
+
+try{
+    //server Setting....
+    $pm->SMTPDebug=SMTP::DEBUG_SERVER;
+    $pm->isSMTP();
+    $pm->Host= "smtp.gmail.com";
+   
+    $pm->Username="jkhouse117@gmail.com";
+    $pm->Password= "jrszqoamitatqehw";
+    $pm->SMTPSecure="tls";
+    $pm->Port=587;
+    $pm->SMTPAuth=true;
+
+
+    ///Recieption......
+    $pm->setFrom("jkhouse117@gmail.com");
+    $pm->addAddress("jkhouse117@gmail.com");
+
+
+    //Content ...
+    $pm->isHTML(true);
+    $pm->Subject="hlw kmn acho tmr ki khobor?";
+    $pm->Body="shakawat kmn ascos?";
+    $pm->AltBody="Allah amar pase thak";
+
+    ///sending critiria
+    $pm->send();
+    echo "Messenge Has been Sent";
+
+
+
+
+}catch (Exception $e){
+    echo "Messenge Failed";    
+}
