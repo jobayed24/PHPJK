f6391e28517f1bf58b5bec8f734dbb5f75f3f720
jobayed24 <jkhouse117@gmail.com>
2021-09-07 03:42:09 +0600

    mistake semicolon or something like this call E_Parse and display errors, error reporting are different by seperately display errors is best for use

---
+++ error/display_error.php
@@ -0,0 +1,7 @@
+<?php
+// ini_set("error_reporting",E_ALL);
+error_reporting(E_ALL);
+// ini_set("display_errors",1);
+
+echo $name;
+echo 9/0;
---
+++ error/error_reporting.php
@@ -0,0 +1,17 @@
+<?php
+// error_reporting(0);
+
+// error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+//accept all types of error without E notice error
+
+// error_reporting(E_ALL & ~E_NOTICE);
+// echo $name;
+
+// error_reporting(-1);
+
+// echo $name;
+
+ini_set("error_reporting",0);
+
+echo $name;
